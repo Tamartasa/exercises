d93ee924de012c18d35308724c3b553081c8c3e8
Tamartasa <116597218+Tamartasa@users.noreply.github.com>
2022-12-08 23:11:09 +0200

    Add files via upload

---
+++ d1.py
@@ -0,0 +1,11 @@
+import os
+
+if __name__ == "__main__":
+
+    def search_csv_files(path) ->list:
+
+path = C:\Users\tamar\Desktop\full stack course\D1- files and directories\username-or-email.csv
+
+
+
+    for (root, dirs, files) in os.walk(
